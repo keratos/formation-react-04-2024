023963b33b51f750a2d08b0903f36a0a6fd9bc81
Jeff <augure@gmail.com>
2024-05-31 16:44:39 +0200

    api

---
+++ module 08 communiquer avec une API FireBase/prj/src/components/Personnes.jsx
@@ -0,0 +1,21 @@
+export default function Personne({ id, personne,effacer,modifier2 }) {
+
+  return (
+    <>
+      <tr className={`table-${personne.etat ? 'success': 'danger'}`}>
+        <td>{personne.prenom}</td>
+        <td>{personne.nom}</td>
+        <td>
+          <button className="btn btn-danger" onClick={()=>effacer(id)}>
+            <i className="fa fa-trash"></i>
+          </button>
+        </td>
+        <td>
+          <button className="btn btn-warning"  onClick={()=>modifier2(id)}>
+            <i className="fa fa-check"></i>
+          </button>
+        </td>
+      </tr>
+    </>
+  );
+}
--- module 08b TP Vip Cocktail/README.md
+++ module 08b TP Vip Cocktail/README.md
@@ -3,3 +3,19 @@
 - Ajouter une personne  
 - Enlever une personne  
 - Modifier avec patch  
+
+```js
+
+let data= {
+    "id1" :{"nom": "PITT", "prenom": "Brad"},
+    "id2" :{"nom": "CAGE", "prenom": "Nicolas"},
+    "id3" :{"nom": "JOLIE", "prenom": "Angelina"}
+}
+
+
+Object.keys(data).map( attribut => {
+  console.log(attribut)
+  //un objet est aussi un tableau
+  console.log(data[attribut]);
+});
+```
